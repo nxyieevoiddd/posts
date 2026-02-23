e6a6ab07951fe1fb8fce4960aa16faf4e9d231cc
nxyieevoiddd <bindris.olivers@gmail.com>
2026-02-23 15:12:40 +0200

    small changes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('welcome
-    ');
+    return view('welcome');
 });
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::POST('/posts', [PostController::class, 'store']);
